3e5829d404303425e08e9ab1eb27a9b0e5d9a340
Brian Toney <btoney1988@gmail.com>
2020-11-09 15:04:11 -0500

    controller js file complete

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -1,5 +1,51 @@
 const express = require("express");
-const burger = require("burger");
+const burger = require("../models/burger");
 
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  burger.all(function (data) {
+    const burgerList = {
+      burgers: data
+    };
+    res.render("index", burgerList);
+  });
+});
+
+router.post("/api/burgers", function (req, res) {
+  burger.insertOne([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function (res) {
+    res.json({ id: res.insertId })
+  });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+  const cond = "id = " + response.params.id;
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, cond, function (res) {
+    if (res.changedRow === 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burgers/:id", function (req, res) {
+  const cond = "id = " + response.params.id;
+
+  burger.delete(cond, function (res) {
+    if (res.affectedRow === 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
 module.exports = router;
